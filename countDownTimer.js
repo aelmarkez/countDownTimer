ec1c2a2a48300b333beeebc96a899a4b6bdeb708
aelmarkez <ahmedelmarkez4@gmail.com>
2021-02-03 14:59:15 +0100

    finalisation

--- countDownTimer.js
+++ countDownTimer.js
@@ -1,22 +1,22 @@
 function getTimeRemaining(endTime){
     var t = Date.parse(endTime) -Date.parse(new Date());
     var seconds = Math.floor((t/1000)%60);
-    var munites = Math.floor(t/(1000*60)%60);
+    var minutes = Math.floor(t/(1000*60)%60);
     var hours = Math.floor(t/(1000*60*60)%24);
     var days = Math.floor(t/(1000*60*60*24));
     return {  'total':t,
             'days' :days,
             'hours' : hours,
-            'minutses' : munites,
+            'minutes' : minutes,
            'seconds'  : seconds 
 };}
 
 function initializeClock(id,endtime){
     var clock = document.getElementById(id);
-    var daysSpan = clock.getElementById('.days');
-    var hoursSpan = clock.getElementById('.hours');
-    var minutesSpan = clock.getElementById('.minutes');
-    var secondsSpan = clock.getElementById('.seconds');
+    var daysSpan = clock.querySelector('.days');
+    var hoursSpan = clock.querySelector('.hours');
+    var minutesSpan = clock.querySelector('.minutes');
+    var secondsSpan = clock.querySelector('.seconds');
 
     function updateClock(){
         var t= getTimeRemaining(endtime);
